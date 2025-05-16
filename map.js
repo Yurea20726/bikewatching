2ea41e44074610adc271fd7b81cb7f68f6fbb518
Yurea20726 <K0978028687@gmail.com>
2025-05-16 06:05:06 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,17 +1,17 @@
 // Import Mapbox as ESM
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-// Set your Mapbox access token here
-mapboxgl.accessToken = 'pk.YOUR_MAPBOX_ACCESS_TOKEN';
+// ✅ 使用你提供的新 token
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hsMzE4IiwiYSI6ImNtYXF0OG4zajAzbXcycW9zZ2llc3l3am0ifQ.xkL6-6l0ERjFfhGWcmz4ag';
 
-// Log to confirm
+// Console 檢查
 console.log('Mapbox GL JS loaded:', mapboxgl);
 
-// Initialize the map
+// 建立地圖
 const map = new mapboxgl.Map({
-  container: 'map', // div id
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [-71.09415, 42.36027], // [longitude, latitude] - centered near MIT
+  container: 'map', // 要綁定的 div id
+  style: 'mapbox://styles/mapbox/streets-v12', // 預設樣式
+  center: [-71.09415, 42.36027], // 波士頓 Cambridge 中心點
   zoom: 12,
   minZoom: 5,
   maxZoom: 18
